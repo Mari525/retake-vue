b9f3d6c33d448c8ada444b6c02f9bfbaa2046e3c
Mari <sherst.neva.maria@ya.ru>
2022-03-28 15:02:21 +0300

    index + about

---
+++ layouts/default.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app dark>
+    <Header />
+    <v-row>
+      <v-col class="col-4 col-md-2">
+        <nav class="d-flex flex-column ml-5">
+          <v-btn outlined class="mb-5">
+            <nuxt-link to="/" class="white--text">Главная</nuxt-link>
+          </v-btn>
+          <v-btn outlined class="mb-5">
+            <nuxt-link to="/about" class="white--text">О нас</nuxt-link>
+          </v-btn>
+          <v-btn outlined>
+            <nuxt-link to="/events" class="white--text">События</nuxt-link>
+          </v-btn>
+        </nav>
+        <div class="mt-10 ml-5">
+          <p class="text-center">Зарегистрировано погодных событий: 
+            <span class="font-weight-bold text-decoration-underline">{{ eventsCount.events }}</span>
+          </p>
+          <v-img :src="require('~/static/left_widget_img.jpg')" width="190px" class="ml-auto mr-auto"></v-img>
+        </div>
+      </v-col>
+      <v-col class="col-10">
+        <nuxt />
+      </v-col>
+    </v-row>
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+
+export default {
+  name: 'DefaultLayout',
+  components: { Header, Footer },
+  data: () => ({
+    eventsCount: []
+  }),
+  async mounted() {
+		this.eventsCount = await this.$axios.$get('https://demo-api.vsdev.space/api/elonus/left_widget')
+	}
+}
+</script>
+
+<style>
+
+a {
+  text-decoration: none;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container class="d-flex">
+    <p class="mr-10 text-justify">{{ indexContent.text }}</p>
+    <v-img :src="indexContent.image" width="300"></v-img>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data: () => ({
+    indexContent: []
+  }),
+  async mounted() {
+		this.indexContent = await this.$axios.$get('https://demo-api.vsdev.space/api/elonus/home_page')
+	}
+}
+</script>
