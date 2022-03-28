b3249d2be95e14367458c6142b9964374a27e6ee
Mari <sherst.neva.maria@ya.ru>
2022-03-28 15:22:54 +0300

    data up

--- api/api.js
+++ api/api.js
@@ -1,5 +1,20 @@
 import axios from "axios";
-import {ApiInformation} from "../utils/constants";
+
+export const ApiRoutes = {
+  GET_MAIN_CONTENT: `/home_page`,
+  GET_SIDE_CONTENT: `/left_widget`,
+  GET_ABOUT_US: `/about_page`,
+  GET_EVENTS: `/events`,
+  GET_FORM: `/events/form`,
+  ADD_EVENT: `/events`,
+};
+
+export const ApiInformation = {
+  BASE_URL: `https://demo-api.vsdev.space/api/elonus`,
+  TIMEOUT: 50000,
+  WITH_CREDENTIALS: false
+};
+
 
 export const createApi = () => {
   const api = axios.create({
